ad9ebef5677f3c3045f51f4ea263505beb9d68ba
William <willspri@gmail.com>
2024-07-01 13:52:58 +1200

    committed

---
+++ toPython.py
@@ -0,0 +1,15 @@
+import os
+
+def rename_files(directory, suffix):
+    for filename in os.listdir(directory):
+        if filename.endswith('.txt'):
+            base = os.path.splitext(filename)[0]
+            new_name = base + suffix
+            os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
+    print("Text files changed to Python files")
+
+# Example usage
+directory_path = os.getcwd()
+#directory_path = 'your_directory_path_here'
+suffix = '.py'
+rename_files(directory_path, suffix)
